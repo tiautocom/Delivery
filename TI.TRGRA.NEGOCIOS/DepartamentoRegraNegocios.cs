822564b363a28ad1859ab45aeb7bdfc02eaeaa3b
tiautocom <tiautocomp@gmail.com>
2021-06-29 09:15:20 -0300

    Atualização webConfigAdm

--- TI.TRGRA.NEGOCIOS/DepartamentoRegraNegocios.cs
+++ TI.TRGRA.NEGOCIOS/DepartamentoRegraNegocios.cs
@@ -124,7 +124,17 @@ namespace TI.REGRA.NEGOCIOS
 
         public DataTable ListaDepartamento()
         {
-            throw new NotImplementedException();
+            try
+            {
+                conexaoSqlServer.LimparParametros();
+                DataTable dadosTabela = new DataTable();
+                dadosTabela = conexaoSqlServer.ExecutarConsulta(CommandType.StoredProcedure, "uspListarDepartamento");
+                return dadosTabela;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
         }
 
         public string GerarIndexDepartamento(int id, string url, string desc, string det, string preco, int cont, string tel)
